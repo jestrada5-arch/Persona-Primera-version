715aba9d32a28da967758f61ef306de239fa3f50
jestrada5-arch <jestrada5@miumg.edu.gt>
2026-02-15 11:43:04 -0600

    se agrego clase persona

---
+++ Persona.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Persona{
+	//atributo
+protected :	
+	string nombres,apellidos,direccion,fecha_nacimiento;
+	int telefono;
+	  
+	//constructores
+	Persona(){
+	}	
+	Persona(string nom,string ape,string dir,int tel, string fn){
+			nombres = nom;
+			apellidos = ape;
+			direccion = dir;
+			telefono = tel; = fn;
+	}
+	
+		void crear();
+		void leer();
+		void actualizar();
+		void borrar();
+};
